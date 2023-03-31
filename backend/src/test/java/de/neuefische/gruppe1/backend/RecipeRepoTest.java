2b1781e1f30ef9ff980a58186c0a8e3e1dad6159
Robin Schatzl <tmai02476@gmail.com>
2023-03-31 10:23:39 +0200

    added RecipeRepoTest and two Tests "getAll_ShouldReturnTrueWhenEmpty() and getAll_ShouldReturnFalseWhenEmpty()"

---
+++ backend/src/test/java/de/neuefische/gruppe1/backend/RecipeRepoTest.java
@@ -0,0 +1,41 @@
+package de.neuefische.gruppe1.backend;
+
+import de.neuefische.gruppe1.backend.model.Recipe;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeRepoTest {
+
+    @Test
+    void getAll_ShouldReturnTrueWhenEmpty() {
+        //GIVEN
+        RecipeRepo recipeRepo = new RecipeRepo();
+
+        //WHEN
+        List<Recipe> actual = recipeRepo.getAll();
+
+        //THEN
+        List<Recipe> expected = new ArrayList<>();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAll_ShouldReturnFalseWhenEmpty() {
+        //GIVEN
+        RecipeRepo recipeRepo = new RecipeRepo();
+
+        //WHEN
+        List<Recipe> actual = recipeRepo.getAll();
+
+        //THEN
+        List<Recipe> expected = new ArrayList<>();
+        expected.add(new Recipe("1", "Pizza"));
+        expected.add(new Recipe("2", "Gurkensalat"));
+
+        assertNotEquals(expected, actual);
+    }
+}
