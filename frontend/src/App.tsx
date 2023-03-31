2df06fe1a6c13da4752fff4a843a1b29a1b88051
Robin Schatzl <tmai02476@gmail.com>
2023-03-31 12:51:59 +0200

    changed App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import RecipeGallery from "./RecipeGallery";
+import axios from "axios";
+import {Recipe} from "./Recipe";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //const{recipe: Recipe[]} = useRe
+    const[recipes, setRecipes] = useState<Recipe[]>([])
+
+
+    useEffect(() => {
+        loadAllRecipes()
+    }, [])
+
+
+    function loadAllRecipes() {
+        axios.get("/api/recipe")
+            .then((getAllRecipesResponse) => {setRecipes(getAllRecipesResponse.data)})
+            .catch((error) =>{console.error(error)})
+    }
+
+    return (
+        <div className="App">
+            <Header/>
+            <RecipeGallery recipes={recipes}/>
+
+
+        </div>
+    );
 }
 
 export default App;
