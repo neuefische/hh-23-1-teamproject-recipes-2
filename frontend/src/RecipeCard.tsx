780fe59d365ee70c35d6ac349ae03d977c9bc379
Robin Schatzl <tmai02476@gmail.com>
2023-03-31 12:50:54 +0200

    added RecipeCard.java

---
+++ frontend/src/RecipeCard.tsx
@@ -0,0 +1,15 @@
+import {Recipe} from "./Recipe";
+
+type RecipeProps = {
+    recipe: Recipe
+}
+
+export default function RecipeCard(props: RecipeProps) {
+
+    return (
+        <div className="recipe-card">
+            {props.recipe.name}
+            {props.recipe.id}
+        </div>
+    )
+}
