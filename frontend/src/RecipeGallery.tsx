a3e1c37052fb6ce7c62f216b7641f4a04f0eb603
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-04-01 16:03:13 +0200

    fixed unused Lines

--- frontend/src/RecipeGallery.tsx
+++ frontend/src/RecipeGallery.tsx
@@ -2,18 +2,15 @@ import {Recipe} from "./Recipe";
 import React from "react";
 import RecipeCard from "./RecipeCard";
 
-
 type RecipeGalleryProps = {
     recipes: Recipe[],
 }
 export default function RecipeGallery(props : RecipeGalleryProps){
     return (
         <div className = "recipe-gallery">
-
             {props.recipes.map((card)=>{
                 return (<RecipeCard key ={card.id}
                     recipe={card}/>)
-
             })}
         </div>
     )
