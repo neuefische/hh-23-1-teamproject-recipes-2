cfc963f46aae0390d600a52fd7d718a346113b36
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-04-01 14:40:50 +0200

    added Recipe add Methode in RecipeService.java

--- backend/src/main/java/de/neuefische/gruppe1/backend/RecipeService.java
+++ backend/src/main/java/de/neuefische/gruppe1/backend/RecipeService.java
@@ -4,12 +4,12 @@ import de.neuefische.gruppe1.backend.model.Recipe;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class RecipeService {
 
     private final RecipeRepo recipeRepo;
-
 
     RecipeService(RecipeRepo recipeRepo) {
         this.recipeRepo = recipeRepo;
@@ -17,5 +17,10 @@ public class RecipeService {
     public List<Recipe> getAll() {
         return recipeRepo.getAll();
 
+    }
+    public Recipe add(Recipe recipeAdd) {
+        String id = UUID.randomUUID().toString();
+        Recipe recipeToAdd = recipeAdd.withId(id);
+        return recipeRepo.add(recipeToAdd);
     }
 }
