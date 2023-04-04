55d54cb9d90f835d1071648c2f4cd191aab60e70
teresakoetter <t.koetter@mailbox.org>
2023-04-04 17:45:33 +0200

    Add method works only with id not with name before reset to working add method

--- frontend/src/AddRecipe.tsx
+++ frontend/src/AddRecipe.tsx
@@ -2,6 +2,7 @@ import {NewRecipe} from "./Recipe";
 import {FormEvent, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
+
 type AddRecipeProps = {
     addRecipe: (newRecipe: NewRecipe) => void
 }
