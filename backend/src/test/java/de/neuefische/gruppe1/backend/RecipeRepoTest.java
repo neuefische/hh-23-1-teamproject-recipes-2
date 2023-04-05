d4b6633b8ba47404c44132a72aeeb9d4f6295fbc
Kathalania <katharina.stern@gmx.net>
2023-04-05 14:28:13 +0200

    Directory resources added to backend/test. application.properties added to backend/test/resources

--- backend/src/test/java/de/neuefische/gruppe1/backend/RecipeRepoTest.java
+++ backend/src/test/java/de/neuefische/gruppe1/backend/RecipeRepoTest.java
@@ -33,8 +33,8 @@ class RecipeRepoTest {
 
         //THEN
         List<Recipe> expected = new ArrayList<>();
-        expected.add(new Recipe("1", "Pizza", "How To Make Pizza"));
-        expected.add(new Recipe("2", "Gurkensalat", "How to Make Gurkensalat"));
+        expected.add(new Recipe("1", "Erstes Rezept, voll gut!"));
+        expected.add(new Recipe("2","Zweites Rezept, noch besser!"));
 
         assertNotEquals(expected, actual);
     }
