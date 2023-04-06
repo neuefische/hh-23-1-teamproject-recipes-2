695839d8dba3730d7fff9d1588feb2fcca44efe1
Robin Schatzl <tmai02476@gmail.com>
2023-04-06 13:46:06 +0200

    delete empty Lines and unused import

--- backend/src/test/java/de/neuefische/gruppe1/backend/RecipeServiceTest.java
+++ backend/src/test/java/de/neuefische/gruppe1/backend/RecipeServiceTest.java
@@ -2,7 +2,6 @@ package de.neuefische.gruppe1.backend;
 
 import de.neuefische.gruppe1.backend.model.Recipe;
 import de.neuefische.gruppe1.backend.model.RecipeRepoInterface;
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,31 +10,21 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-
-import java.beans.Beans;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-
 
 @AutoConfigureMockMvc
 @SpringBootTest
 
-
 public class RecipeServiceTest {
 
-
-
     private RecipeService recipeService;
-
     @Mock
     private RecipeRepoInterface recipeRepoInterfaceMock;
-
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
@@ -58,7 +47,6 @@ public class RecipeServiceTest {
         for (int i = 0; i < expectedRecipes.size(); i++) {
             Assertions.assertEquals(expectedRecipes.get(i), actualRecipes.get(i));
         }
-
         verify(recipeRepoInterfaceMock, times(1)).findAll();
     }
     @Test
@@ -73,6 +61,7 @@ public class RecipeServiceTest {
         //WHEN
         List<Recipe> actual = recipeService.getAll();
         List<Recipe> expected = new ArrayList<>();
+
         //THEN
         verify(recipeRepoInterface).findAll();
         assertEquals(actual, expected);
@@ -96,12 +85,4 @@ public class RecipeServiceTest {
         verify(recipeRepoInterface).save(pizzaFunghi);
         assertEquals(actual, pizzaFunghi);
     }
-
 }
-
-
-
-
-
-
-
