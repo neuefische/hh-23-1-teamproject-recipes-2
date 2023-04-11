18631af8d33a4498baf6f0a2a6d22c0b05b694e0
Kathalania <katharina.stern@gmx.net>
2023-04-11 14:42:57 +0200

    Password hashed via jshell console. getMe method added to UserController.java

--- backend/src/main/java/de/neuefische/gruppe1/backend/security/UserController.java
+++ backend/src/main/java/de/neuefische/gruppe1/backend/security/UserController.java
@@ -1,15 +1,41 @@
 package de.neuefische.gruppe1.backend.security;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/recipes/users")
 
 public class UserController {
     @GetMapping
-    public String getTest(){
+    public String getTest() {
         return "Test";
     }
+
+/*    @GetMapping("/me")
+    public String getMe(Principal principal) {
+        System.out.println(principal);
+        return principal.getName();
+    }*/
+
+/*    @GetMapping("/me")
+    public String getMe(Principal principal) {
+        if (principal == null) {
+            return "anonymousUser";
+        }
+        return principal.getName();
+    }*/
+
+    @GetMapping("/me")
+    public String getMe() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
+    }
+
 }
