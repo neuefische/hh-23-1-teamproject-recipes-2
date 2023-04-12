4a95653dc1043ecc7887e03640ec51d40ac65f61
Robin Schatzl <tmai02476@gmail.com>
2023-04-12 17:29:37 +0200

    addded IntegrationTest for addRecipe

--- backend/src/test/java/de/neuefische/gruppe1/backend/RecipeIntegrationTest.java
+++ backend/src/test/java/de/neuefische/gruppe1/backend/RecipeIntegrationTest.java
@@ -7,10 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,6 +16,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class RecipeIntegrationTest {
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    RecipeRepoInterface recipeRepoInterface;
+
 
     @Test
     void getAll_ShouldReturnAllRecipes() throws Exception {
