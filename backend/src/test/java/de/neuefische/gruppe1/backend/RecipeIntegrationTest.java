984685bc53cb93f47176e60e2c12367818ac0724
Robin Schatzl <tmai02476@gmail.com>
2023-04-13 11:03:40 +0200

    added IntegrationTest for getAll with AnonymousUser

--- backend/src/test/java/de/neuefische/gruppe1/backend/RecipeIntegrationTest.java
+++ backend/src/test/java/de/neuefische/gruppe1/backend/RecipeIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,8 +24,20 @@ class RecipeIntegrationTest {
     MockMvc mockMvc;
 
     @Test
+    @WithAnonymousUser
+    void getAll_ShouldReturnAllRecipesWithAnonymousUser() throws Exception {
+        mockMvc.perform(get("/api/recipes"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                []
+                                """
+                ));
+    }
+
+    @Test
     @WithMockUser
-    void getAll_ShouldReturnAllRecipes() throws Exception {
+    void getAll_ShouldReturnAllRecipesWithMockUser() throws Exception {
         mockMvc.perform(get("/api/recipes"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(
