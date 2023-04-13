55f05b15c6264b83b7e16b22d0deb93cb9185548
teresakoetter <t.koetter@mailbox.org>
2023-04-13 10:57:51 +0200

    Push für WithMockUserTest

--- backend/src/test/java/de/neuefische/gruppe1/backend/RecipeIntegrationTest.java
+++ backend/src/test/java/de/neuefische/gruppe1/backend/RecipeIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -22,6 +23,7 @@ class RecipeIntegrationTest {
     MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     void getAll_ShouldReturnAllRecipes() throws Exception {
         mockMvc.perform(get("/api/recipes"))
                 .andExpect(status().isOk())
