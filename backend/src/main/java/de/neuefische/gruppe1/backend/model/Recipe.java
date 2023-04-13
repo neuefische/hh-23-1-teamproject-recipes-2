8aebb67ac0ce2cbe5108e37c8f73aa0106464eb5
Robin Schatzl <tmai02476@gmail.com>
2023-04-13 15:32:07 +0200

    add validation in Recipe.java with @NotBlank and @Size in name and description

--- backend/src/main/java/de/neuefische/gruppe1/backend/model/Recipe.java
+++ backend/src/main/java/de/neuefische/gruppe1/backend/model/Recipe.java
@@ -1,11 +1,17 @@
 package de.neuefische.gruppe1.backend.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.annotation.Id;
 
 public record Recipe(
         @Id
         String id,
+        @NotBlank
+        @Size(min=3, max=100)
         String name,
+        @NotBlank
+        @Size(min=30)
         String description
 ) {
 }
