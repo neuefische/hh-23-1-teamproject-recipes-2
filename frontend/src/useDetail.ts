27103960db37f2326861a39cd7a04095c8846c4a
Robin Schatzl <tmai02476@gmail.com>
2023-04-18 17:35:51 +0200

    bug fix

--- frontend/src/useDetail.ts
+++ frontend/src/useDetail.ts
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {Recipe} from "./Recipe";
-import {useNavigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {toast} from "react-toastify";
 import axios from "axios";
 
