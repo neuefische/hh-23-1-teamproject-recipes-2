1cad4df4d2a1f65eaee522d8506d93b93c598480
teresakoetter <t.koetter@mailbox.org>
2023-04-19 18:07:09 +0200

    final design (exclusively width of description field)

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
     return (
         <div className="header">
             <h1>Just eat it.</h1>
-            <p>"no one wants to be defeated- showin' how funky and strong is your fight -
-                it doesn't matter who's wrong or right, just eat it, eat it"</p>
+            <h2>"no one wants to be defeated- showin' how funky and strong is your fight -
+                it doesn't matter who's wrong or right, just eat it, eat it"</h2>
             <div className="navbar">
             <Link to="/recipes">Zu den Rezepten</Link><br/>
             <NavLink to="/recipes/add">Rezept erstellen</NavLink><br/>
