c55f63981f1d99d8652c1ca9b304ea74869ad6c1
Robin Schatzl <tmai02476@gmail.com>
2023-04-19 16:26:13 +0200

    added Search Bar

--- frontend/src/RecipeGallery.tsx
+++ frontend/src/RecipeGallery.tsx
@@ -2,12 +2,14 @@ import {Recipe} from "./Recipe";
 import React, {useState} from "react";
 import RecipeCard from "./RecipeCard";
 import './RecipeGallery.css'
+import useRecipes from "./useRecipes";
 
 type RecipeGalleryProps = {
     recipes: Recipe[],
 }
 export default function RecipeGallery(props: RecipeGalleryProps) {
     const [searchTerm, setSearchTerm] = useState("");
+    const {recipes} = useRecipes()
 
     const filteredRecipes = recipes.filter((recipe) =>
         recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -20,13 +22,9 @@ export default function RecipeGallery(props: RecipeGalleryProps) {
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
-            {filteredRecipes.map((recipe : Recipe) => (
-                <RecipeCard key={recipe.id} recipe={recipe} />
+            {filteredRecipes.map((card : Recipe) => (
+                <RecipeCard key={card.id} recipe={card} />
             ))}
-            {props.recipes.map((card) => {
-                return (<RecipeCard key={card.id}
-                                    recipe={card}/>)
-            })}
         </div>
     )
 }
