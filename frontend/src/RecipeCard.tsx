5fbedf0d900e4c8586e95d0fa57ff9e67993df7c
teresakoetter <t.koetter@mailbox.org>
2023-04-19 16:40:19 +0200

    erste Version Design vor main merge in Design

--- frontend/src/RecipeCard.tsx
+++ frontend/src/RecipeCard.tsx
@@ -12,13 +12,12 @@ export default function RecipeCard(props: RecipeProps) {
 
     return (
         <div className="recipe-card">
-            <p>Name:</p>
             {props.recipe.name}
-            <p>Description:</p>
+            <br/><br/>
+            <p>weitere Infos:</p>
             {props.recipe.description}
-            <p>ID:</p>
-            {props.recipe.id}
-            <button onClick={() => {navigate("/recipes/" + props.recipe.id)}}>Recipe Details</button>
+            <br/><br/>
+            <button onClick={() => {navigate("/recipes/" + props.recipe.id)}}>Rezeptdetails</button>
         </div>
     )
 }
